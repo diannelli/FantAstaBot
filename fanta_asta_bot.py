70daa87f069079c40884fd67417a64c8f57bfa57
INPECO\damiano.iannelli <damiano.iannelli@inpeco.com>
2018-08-10 17:34:58 +0200

    added ConversationHandler

--- fanta_asta_bot.py
+++ fanta_asta_bot.py
@@ -1,8 +1,12 @@
-from telegram.ext import Updater
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, bot, update,ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler
+from telegram.ext import ConversationHandler, CallbackQueryHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import logging
 import logging.config
-import yaml
+
+MAKE_OFFERS, READ_OFFERS = range(2)
+
 
 def set_logging():
     '''Logging configuration with a YAML format configuration file.
@@ -13,37 +17,74 @@ def set_logging():
     flogger = get_flogger()
     return flogger
 
+
 def get_flogger():
     '''Returns a flogger instance'''
     flogger = logging.getLogger('flogger')
     return flogger
 
-def build_menu(buttons,
-               n_cols,
-               header_buttons=None,
-               footer_buttons=None):
-    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
-    if header_buttons:
-        menu.insert(0, header_buttons)
-    if footer_buttons:
-        menu.append(footer_buttons)
-    return menu
+
+def start(bot, update):
+    """
+    Start function. Displayed whenever the /start command is called.
+    This function sets the language of the bot.
+    """
+    # Create buttons to slect language:
+    keyboard = [['FAI OFFERTA', 'CONSULTA OFFERTE']]
+
+    # Create initial message:
+    message = "Hey, I'm FantAstaBot"
+
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+    update.message.reply_text(message, reply_markup=reply_markup)
+
+    return SET_LANG
+
+
+def set_lang(bot, update):
+    """
+    First handler with received data to set language globally.
+    """
+    # Set language:
+    global LANG
+    LANG = update.message.text
+    user = update.message.from_user
+
+    # logger.info("Language set by {} to {}.".format(user.first_name, LANG))
+    update.message.reply_text(lang_selected[LANG],
+                            reply_markup=ReplyKeyboardRemove())
+
+    return MENU
+
 
 f = open('token.txt', 'r')
 updater = Updater(token=f.readline())
 f.close()
-#
-# dispatcher = updater.dispatcher
-#
-# logger = set_logging()
-# updater.start_polling()
-# logger.info('Bet_Bot started.')
-# updater.idle()
-chat_id = update.Message.chat_id
 
-custom_keyboard = [["top-left", "top-right"],
-                   ["bottom-left", "bottom-right"]]
-reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-updater.bot.send_message(chat_id=chat_id,
-                 text="Custom Keyboard Test",
-                 reply_markup=reply_markup)
+# Get the dispatcher to register handlers:
+dp = updater.dispatcher
+
+conv_handler = ConversationHandler(
+    entry_points=[CommandHandler('start', start)],
+
+    states={
+        SET_LANG: [CommandHandler('set_lang', set_lang)]
+    },
+
+    fallbacks=[CommandHandler('cancel', set_lang),
+               CommandHandler('help', help)]
+)
+
+dp.add_handler(conv_handler)
+
+# Log all errors:
+# dp.add_error_handler(error)
+
+# Start DisAtBot:
+updater.start_polling()
+
+# Run the bot until the user presses Ctrl-C or the process
+# receives SIGINT, SIGTERM or SIGABRT:
+updater.idle()
